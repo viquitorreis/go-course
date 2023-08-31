f947cdcf2ee4422b058087369b7fee373eeb5023
victor <victorreisprog@gmail.com>
2023-08-31 16:48:06 -0300

    commit #8 - maps terminado

---
+++ go-free-code-camp/course/9-maps/3-maps-count.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+)
+
+func getCounts(userIDs []string) map[string]int {
+	counts := make(map[string]int)
+
+	for _, userID := range userIDs {
+		count := counts[userID]
+		count++
+		counts[userID] = count
+	}
+
+	return counts
+}
+
+func test(userIDs []string, ids []string) {
+	fmt.Printf("Generating counts for %v user IDs...\n", len(userIDs))
+
+	counts := getCounts(userIDs)
+	fmt.Println("Counts from select IDs:")
+	for _, k := range ids {
+		v := counts[k]
+		fmt.Printf(" - %s: %d\n", k, v)
+	}
+	fmt.Println("=====================================")
+}
+
+func main() {
+	userIDs := []string{}
+	for i := 0; i < 10000; i++ {
+		h := md5.New()
+		io.WriteString(h, fmt.Sprint(i))
+		key := fmt.Sprintf("%x", h.Sum(nil))
+		userIDs = append(userIDs, key[:2])
+	}
+
+	test(userIDs, []string{"00", "ff", "dd"})
+	// test(userIDs, []string{"aa", "12", "32"})
+	// test(userIDs, []string{"bb", "33"})
+}

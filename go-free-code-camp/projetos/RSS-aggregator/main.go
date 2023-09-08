6910300229e417982ce85bef84b18e51bd63d34a
victor <victorreisprog@gmail.com>
2023-09-08 14:01:11 -0300

    commit #17 - metodo de create e get user by API key

--- go-free-code-camp/projetos/RSS-aggregator/main.go
+++ go-free-code-camp/projetos/RSS-aggregator/main.go
@@ -1,23 +1,45 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"gitlab.com/victorreisprog/go/-/tree/master/go-free-code-camp/projetos/RSS-aggregator/internal/database"
+
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 
 	godotenv.Load(".env")
 
 	portStr := os.Getenv("PORT")
-
 	if portStr == "" {
 		log.Fatal("PORTA não encontrada no .env") // vai parar o programa imediatamente com codigo 1 e uma mensagem
+	}
+
+	dbUrl := os.Getenv("DB_URL")
+	if dbUrl == "" {
+		log.Fatal("DB_URL não encontrada no .env")
+	}
+	dbConn, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal("Banco não conectou corretamente: %v", err)
+	}
+
+	convQueries := database.New(dbConn)
+	apiCfg := apiConfig{ // estpa apontando para nossa struct apiConfig
+		DB: convQueries,
 	}
 
 	router := chi.NewRouter()
@@ -34,6 +56,8 @@ func main() {
 	v1router := chi.NewRouter()
 	v1router.Get("/healthz", handlerReadiness)
 	v1router.Get("/err", handlerErr)
+	v1router.Post("/users", apiCfg.handlerCreateUser)
+	v1router.Get("/users", apiCfg.handlerGetUserByAPIKey)
 
 	router.Mount("/v1", v1router)
 
@@ -43,7 +67,7 @@ func main() {
 	}
 
 	log.Printf("Server está inicializando na porta %v", portStr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
